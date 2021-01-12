d095034c4c68a31fe61c177716dde706c69ee0f5
Jay Patel <j.patel8797@gmail.com>
2021-01-11 19:51:29 -0800

    Addition of file BUYBOT.py

---
+++ BUYBOT.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+# Abstract class BUYBOT
+# Used as a template for AMZN_BOT, BSTBUY_BOT, NEWEGG_BOT 
+class BUY_BOT(ABC) : 
+    @abstractmethod
+    def setup(self) : 
+        pass
+
+    @abstractmethod
+    def check_in_stock(self) :
+        pass
+
+    @abstractmethod
+    def add_to_cart(self) :  
+        pass
+
+    @abstractmethod
+    def checkout(self) : 
+        pass
