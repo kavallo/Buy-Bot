1359b10b12e6b35e8e7c70b0c5129a7484bdd08f
Jay Patel <j.patel8797@gmail.com>
2021-01-26 23:21:29 -0800

    Added command that installs asyncIO

--- setup.bat
+++ setup.bat
@@ -6,5 +6,8 @@ cmd /c "pip install selenium"
 @ECHO Installing dotENV...
 cmd /c "pip install python-dotenv"
 
+@ECHO Installing asyncIO...
+cmd /c "pip install asyncio"
+
 @ECHO Process finished. Please edit .env file before running start.bat  
 @pause
