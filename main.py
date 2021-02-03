9cbd31b205c56758efac91b1ea14255c0e3bfd81
Jay Patel <j.patel8797@gmail.com>
2021-02-03 14:23:26 -0800

    Setup now utilizes USE_ env variables + threading

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from BSTBUY import BSTBUY_BOT
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from dotenv import load_dotenv
-import asyncio
 import threading
 import time
 import os
 
 load_dotenv() 
+USE_AMZN = os.getenv("USE_AMZN")
+USE_BSTBUY = os.getenv("USE_BSTBUY")
 
 # ------------------------------------------
 # Webdriver setup
@@ -27,12 +28,32 @@ BESTBUY = BSTBUY_BOT(webdriver.Chrome(BSTBUY_DRIVER_PATH, options=chrome_options
 # Functions
 
 # Perform setup for BstBuy & Amzn bots
-async def setup() : 
-    AMZN_SETUP = setup_loop.create_task(AMAZON.setup())
-    BB_SETUP = setup_loop.create_task(BESTBUY.setup())
-    await asyncio.wait([AMZN_SETUP, BB_SETUP])
-
+def setup() :
+    # If "USE_AMZN" setting is set to True in .env file
+    if(USE_AMZN) :
+        AMZN_SETUP_THREAD = threading.Thread(target=AMAZON.setup())
+        AMZN_SETUP_THREAD.start()
+    else :
+        AMAZON.close()
+        print("[AMAZON] Bot not in use. To fix, change settings in .env file")
+    
+    # If "USE_BSTBUY" setting is set to True in .env file
+    if(USE_BSTBUY) :
+        BSTBUY_SETUP_THREAD = threading.Thread(target=BESTBUY.setup())
+        BSTBUY_SETUP_THREAD.start()
+    else : 
+        BESTBUY.close()
+        print("[BESTBUY] Bot not in use. To fix, change settings in .env file")
+    # If both bots are set to False in .env file
+    if ((not USE_AMZN) and (not USE_BSTBUY)) :
+        print("Both bots set to \"False\" in .env file. Exiting...")
+        exit()
+    # Join setup threads 
+    AMZN_SETUP_THREAD.join() 
+    BSTBUY_SETUP_THREAD.join()
+    
 # Checks if the product is in stock 
+# Returns boolean based on which bot has the item in stock
 def stock_check() :
     # Loop until product is in stock 
     while (True) :
@@ -52,19 +73,17 @@ def stock_check() :
         else : 
             print("Sleeping " + os.getenv("page_refresh_timer") + " seconds...")
             time.sleep(int(os.getenv("page_refresh_timer")))
-            
-# ------------------------------------------
-# Main
 
-# Setup 
-setup_loop = asyncio.get_event_loop()
-setup_loop.run_until_complete(setup())
+# Runs setup, checks if the item is in stock, then purchases product 
+def main() : 
+    setup()
+    check = stock_check()
+    if (check) : 
+        AMAZON.checkout()
+    else : 
+        BESTBUY.checkout()
 
-# Stock checking
-check = stock_check()
+# -------------------------
+# Run 
 
-# Checkout
-if (check) : 
-    AMAZON.checkout()
-else : 
-    BESTBUY.checkout()
+main() 
